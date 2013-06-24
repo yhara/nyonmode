e223e294eafc5fa4d4626cfbf0a39c7e44c30445
Yutaka HARA <yutaka.hara.gmail.com>
2013-06-24 11:59:47 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nyonmode::Application.config.secret_key_base = '3d2ef5bb846041ba7afd3d2fa7ab9bddf7caab8262d9a6473b31f82515078bec4f48797fe37c458a0812158c60a7184500a2adb559eb0984c2147f5ae21580fc'
